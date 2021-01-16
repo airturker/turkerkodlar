47b901c56081c8d1d75a7694959821b61af8869d
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-17 02:16:50 +0300

    Add project files.

---
+++ turkerkodlar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace turkerkodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
