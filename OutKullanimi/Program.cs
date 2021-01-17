0e4b401cdf21ef5905baa0dbe2c390f376616c00
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-17 16:47:59 +0300

    odev3

---
+++ OutKullanimi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OutKullanimi
+{
+	class OutParametre
+	{
+		int uc, bes = 0;
+		public int GetSayi(int n, out int sayi)
+		{
+
+			for (int i = 1; i <= n; i++)
+			{
+
+				if (i % 3 == 0)
+					uc++;
+
+				if (i % 5 == 0)
+					bes++;
+			}
+			sayi = bes;
+
+			return uc;
+		}
+	}
+
+	class OutOrnek
+	{
+		static void Main()
+		{
+			OutParametre op = new OutParametre();
+			int a, b;
+
+			a = op.GetSayi(25, out b);
+
+			System.Console.WriteLine("Üçe bölünen sayısı: " + a);
+			System.Console.WriteLine("Beşe bölünen sayısı: " + b);
+		}
+	}
+}
---
+++ RefKullanimi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefKullanimi
+{
+    class Referans
+    {
+        public void Kare(ref int i, ref int k)
+        {
+            i = i * k;
+        }
+    }
+
+    class RefOrnek
+    {
+        static void Main()
+        {
+            Referans rf = new Referans();
+
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("Çağrıdan önce: " + a);
+
+            rf.Kare(ref a, ref b);
+
+            Console.WriteLine("Çağrıdan sonra: " + a);
+        }
+    }
+}
