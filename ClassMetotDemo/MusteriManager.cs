cabc4cb0a7d9033a0044ed6dbb48fdeaf08b8b9f
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-17 17:03:19 +0300

    ödev-3-2

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri eklendi! : " + musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri silindi! : " + musteri.Adi);
+        }
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteriler listelendi! : " + musteri.Adi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "M.Türker";
+            musteri1.Soyadi = "Özakıncı";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Engin";
+            musteri2.Soyadi = "Demiroğ";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Adi = "Ali";
+            musteri3.Soyadi = "Veli";
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(musteri1);
+            musterimanager.Sil(musteri2);
+            musterimanager.Listele(musteri3);
+        }
+    }
+}
