c2a06762ce82f4ba7d9950a101098b2fd7b1282c
Mehmet Türker ÖZAKINCI <airtur@hotmail.com>
2021-01-21 22:37:17 +0300

    ödev 4

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> isimler = new MyDictionary<int, string>();
+            isimler.Add(101, "Engin");
+            isimler.Add(102, "Kerem");
+            isimler.Add(103, "Türker");
+            isimler.Add(104, "Selami");
+
+            
+
+        }
+    }
+}
